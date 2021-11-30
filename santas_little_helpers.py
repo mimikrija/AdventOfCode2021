347fef71e099418c1dadb864b42281fdd0f41c83
Maja Gacesa <maja.gacesa@ansys.com>
2021-11-30 21:03:15 +0100

    add option to convert to int

--- santas_little_helpers.py
+++ santas_little_helpers.py
@@ -1,8 +1,15 @@
-def get_input(file_name, separator='\n'):
+
+def get_input(file_name, numbers=False, separator='\n'):
     """ Reads in data present in `file_name` separated by `separator` string. """
     with open(file_name) as input_file:
         input_data = input_file.read().split(separator)
-    return input_data
+    if numbers:
+        try:
+            return list(map(int, input_data))
+        except:
+            pass
+    else:
+        return input_data
 
 def print_solutions(part_1, part_2 = None):
     print(f'Part 1 solution is: {part_1}')
