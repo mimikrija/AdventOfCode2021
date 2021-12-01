07d1c04798d887c4b267a42817b66f945c3e2631
Maja Gacesa <maja.gacesa@ansys.com>
2021-12-01 20:56:23 +0100

    [day01] write it more nicely thanks Miran

--- 01.py
+++ 01.py
@@ -4,7 +4,7 @@ from santas_little_helpers import *
 
 sea_depths = get_input('inputs/01.txt', True)
 
-party_1 = sum(first > second for first, second in zip(sea_depths[1:], sea_depths))
+party_1 = sum(second > first for first, second in zip(sea_depths, sea_depths[1:]))
 party_2 = sum(sum(sea_depths[n+1:n+4]) > sum(sea_depths[n:n+3]) for n, depth in enumerate(sea_depths[:-3]))
 
 print_solutions(party_1, party_2)
