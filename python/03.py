ea1f07b20f39cb0fac028988095b129fee551695
Maja Gacesa <maja.gacesa@ansys.com>
2021-12-03 07:03:59 +0100

    [day03] first attempt part 2, eww

--- python/03.py
+++ python/03.py
@@ -6,6 +6,9 @@ consumption = lambda gamma, epsilon: gamma * epsilon
 
 data = get_input('inputs/03.txt')
 
+size = len(data[0])
+
+
 gamma = ''
 epsilon = ''
 for stupac in zip(*data):
@@ -16,4 +19,41 @@ gamma = int(gamma, 2)
 epsilon = int(epsilon, 2)
 party_1 = consumption(gamma, epsilon)
 
-print_solutions(party_1)
+
+
+oxygen = list(data)
+scrubber = list(data)
+# party 2
+
+
+scrubber = list(data)
+for column in range(size):
+    stupac = ''.join(c[column] for c in scrubber)
+    if len(scrubber) > 1:
+        most = Counter(stupac).most_common()[0][0]
+        least = Counter(stupac).most_common()[-1][0]
+        if stupac.count('1') == stupac.count('0'):
+            most = '1'
+
+        temp = list(scrubber)
+        scrubber = [number for number in temp if number[column] == most]
+
+
+
+oxygen = list(data)
+for column in range(size):
+    stupac = ''.join(c[column] for c in oxygen)
+    if len(oxygen) > 1:
+        least = Counter(stupac).most_common()[-1][0]
+        if stupac.count('1') == stupac.count('0'):
+            least = '0'
+        temp = list(oxygen)
+        oxygen = [number for number in temp if number[column] == least]
+print(oxygen, scrubber)
+scrubber = int(scrubber[0],2)
+oxygen = int(oxygen[0], 2)
+print(scrubber, oxygen)
+
+party_2 = consumption(scrubber, oxygen)
+
+print_solutions(party_1, party_2)
