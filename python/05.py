c2e63a39cf76c248a96a5fff2a557c6f32eb1562
Maja Gacesa <maja.gacesa@ansys.com>
2021-12-05 08:07:10 +0100

    [day05] part 2 first go :(

--- python/05.py
+++ python/05.py
@@ -2,19 +2,30 @@ from santas_little_helpers import *
 from collections import Counter
 
 data = get_input('inputs/05.txt')
-data = get_input('inputs/05-ex.txt')
+#data = get_input('inputs/05-ex.txt')
+
+direction = lambda start, end: 1 if end > start else -1
 
 def set_from_ends(starts, ends):
     s_x, s_y = starts
     e_x, e_y = ends
-    if s_x > e_x:
+    if s_x > e_x or s_y > e_y:
         s_x, e_x = e_x, s_x
-    if s_y > e_y:
         s_y, e_y = e_y, s_y
+    nums = set()
+
     if s_x == e_x or s_y == e_y:
         return {(x, y) for x in range(s_x, e_x+1) for y in range(s_y, e_y+1)}
-    else:
-       return set()
+    elif abs((s_x - e_x)/(s_y - e_y)) == 1:
+        dx = direction(s_x, e_x)
+        dy = direction(s_y,e_y)
+        return {(x,  y) for x, y in zip(range(s_x, e_x+dx, dx), range(s_y, e_y+dy, dy))}
+        #     print(x,y)
+        # if not nums:
+        #     nums = {(x, y) for x,  y in zip(range(s_x, e_x+1), range(s_y, e_y+1))}
+        # return {(x, y) for x,  y in zip(range(s_x, e_x+1), range(s_y, e_y+1))}
+
+    #return nums
 
 all_numbers = set()
 party_1 = 0
