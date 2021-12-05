bc7889e339977f3dc83c1be5b5ac8e03e07c706c
Maja Gacesa <maja.gacesa@ansys.com>
2021-12-05 08:25:55 +0100

    [day05] clean up

--- python/05.py
+++ python/05.py
@@ -1,57 +1,44 @@
+# Day 5: Hydrothermal Venture
+
 from santas_little_helpers import *
 from collections import Counter
 
 data = get_input('inputs/05.txt')
-#data = get_input('inputs/05-ex.txt')
 
+# if we need a range in descending order this helps
 direction = lambda start, end: 1 if end > start else -1
 
-def set_from_ends(starts, ends):
-    s_x, s_y = starts
-    e_x, e_y = ends
-    if s_x > e_x or s_y > e_y:
-        s_x, e_x = e_x, s_x
-        s_y, e_y = e_y, s_y
-    nums = set()
+is_hor_ver = lambda sx, ex, sy, ey: sx == ex or sy == ey
+is_45 = lambda sx, ex, sy, ey: abs((sx - ex)/(sy - ey)) == 1
 
-    if s_x == e_x or s_y == e_y:
-        return {(x, y) for x in range(s_x, e_x+1) for y in range(s_y, e_y+1)}
-    elif abs((s_x - e_x)/(s_y - e_y)) == 1:
-        dx = direction(s_x, e_x)
-        dy = direction(s_y,e_y)
-        return {(x,  y) for x, y in zip(range(s_x, e_x+dx, dx), range(s_y, e_y+dy, dy))}
-        #     print(x,y)
-        # if not nums:
-        #     nums = {(x, y) for x,  y in zip(range(s_x, e_x+1), range(s_y, e_y+1))}
-        # return {(x, y) for x,  y in zip(range(s_x, e_x+1), range(s_y, e_y+1))}
+def line_from_endpoints(starts, ends, diagonals=False):
+    sx, sy = starts
+    ex, ey = ends
+    dx = direction(sx, ex)
+    dy = direction(sy, ey)
 
-    #return nums
+    if is_hor_ver(sx, ex, sy, ey):
+        return [(x, y) for x in range(sx, ex+dx, dx) for y in range(sy, ey+dy, dy)]
+    elif is_45(sx, ex, sy, ey) and diagonals:
+        return [(x,  y) for x, y in zip(range(sx, ex+dx, dx), range(sy, ey+dy, dy))]
+    else:
+        return list()
 
-all_numbers = set()
-party_1 = 0
-ind_size = 0
-all_nums_list = []
+def count_overlaps(end_points, diagonals=False):
+    all_points = []
+    for starts, ends in end_points:
+        all_points += line_from_endpoints(starts, ends, diagonals)
+    points_count = Counter(all_points)
+    return sum(freq >=2 for _, freq in points_count.most_common())
+
+end_points = []
 for line in data:
-
     start, end = line.split('->')
-    starts = (int(num) for num in start.split(','))
-    ends = (int(num) for num in end.split(','))
-    add_set = set_from_ends(starts, ends)
-    all_nums_list += list(add_set)
+    starts = tuple(int(num) for num in start.split(','))
+    ends = tuple(int(num) for num in end.split(','))
+    end_points.append(tuple((starts, ends)))
 
 
-    all_numbers |= add_set
-    ind_size += len(add_set)
+party_1, party_2 = (count_overlaps(end_points, diagonals) for diagonals in (False, True))
 
-    # print(len(all_numbers), ind_size)
-
-points_count = Counter(all_nums_list)
-#print(points_count)
-for coord, count in points_count.most_common():
-    if count >= 2:
-        party_1 += 1
-
-
-print_solutions(party_1)
-
-# not 6167
+print_solutions(party_1, party_2)
