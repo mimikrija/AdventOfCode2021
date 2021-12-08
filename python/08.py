2882397af947b019aa33125112c655dd9b122e20
Maja Gacesa <maja.gacesa@ansys.com>
2021-12-08 17:00:11 +0100

    [day08] clean up a bit (I still don't like it)

--- python/08.py
+++ python/08.py
@@ -1,14 +1,9 @@
+# Day 8: Seven Segment Search
 
 from santas_little_helpers import *
 from collections import defaultdict
-data = get_input('inputs/08.txt')
-#data = get_input('inputs/08-ex.txt')
 
-
-displays = []
-for line in data:
-    signal_pattern, digits = [line.split('|')[pos].split() for pos in {0,1}]
-    displays.append((signal_pattern, digits))
+is_easy = lambda x: 2 <= len(x) <= 4 or len(x) == 7
 
 def decypher(patterns):
     DIGIT_SIZE = {
@@ -26,11 +21,13 @@ def decypher(patterns):
     SIZE_DIGIT = defaultdict(set)
     for digit, size in DIGIT_SIZE.items():
         SIZE_DIGIT[size].add(digit)
+
     # create dict
     unknown = defaultdict(set)
     for pattern in patterns:
         unknown[len(pattern)].add(pattern)
     translate = dict()
+
     # find easy ones
     for size, patterns in unknown.items():
         if len(patterns) == 1:
@@ -48,9 +45,6 @@ def decypher(patterns):
             translate[6] = candidate
             unknown[DIGIT_SIZE[6]].remove(candidate)
             break
-    
-
-  
     # find 9
     for candidate in unknown[DIGIT_SIZE[9]]:
         if all(c in candidate for c in translate[4]):
@@ -59,7 +53,6 @@ def decypher(patterns):
             break
     # find 0
     translate[0] = unknown[DIGIT_SIZE[0]].pop()
-
     # find 5
     for candidate in unknown[DIGIT_SIZE[5]]:
         if all(c in translate[6] for c in list(candidate)):
@@ -71,22 +64,31 @@ def decypher(patterns):
 
     final = dict()
     for digit, code in translate.items():
-        codes = ''.join(c for c in sorted(code))
-        final[codes] = digit
-
+        sorted_code = ''.join(c for c in sorted(code))
+        final[sorted_code] = digit
     return final
 
 
+data = get_input('inputs/08.txt')
+
+
+displays = []
+for line in data:
+    signal_pattern, digits = [line.split('|')[pos].split() for pos in {0,1}]
+    displays.append((signal_pattern, digits))
+
+
+party_1 = sum(is_easy(digit) for _, digits in displays for digit in digits)
 
 party_2 = 0
 for patterns, digits in displays:
     solution = ''
+    translator = decypher(patterns)
     for code in digits:
         scode = ''.join(c for c in sorted(code))
-        solution += str(decypher(patterns)[scode])
+        solution += str(translator[scode])
     party_2 += int(solution)
 
-is_easy = lambda x: 2 <= len(x) <= 4 or len(x) == 7
-party_1 = sum(is_easy(digit) for _, digits in displays for digit in digits)
+
 
 print_solutions(party_1, party_2)
