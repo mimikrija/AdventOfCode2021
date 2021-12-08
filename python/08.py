30b7bc0445a2aba282df1337381b7d66e7cd1f8e
Maja Gacesa <maja.gacesa@ansys.com>
2021-12-08 06:27:12 +0100

    [day08] first go

--- python/08.py
+++ python/08.py
@@ -1,6 +1,19 @@
 
 from santas_little_helpers import *
-data = get_input('inputs/08.txt', True,',')
-data = get_input('inputs/08-ex.txt', True,',')
-print(data[:5])
-#print_solutions(party_1)
+data = get_input('inputs/08.txt')
+#data = get_input('inputs/08-ex.txt')
+easy_data = [line.split('|')[1] for line in data]
+counter = 0
+for line in easy_data:
+    for digit in line.split():
+        if len(digit) == 2:
+            counter += 1
+        if len(digit) == 4:
+            counter += 1
+        if len(digit) == 3:
+            counter += 1
+        if len(digit) == 7:
+            counter += 1
+
+party_1 = counter
+print_solutions(party_1)
