e256e408f75ff073371ad53f292fb5f22d539f79
Maja Gacesa <maja.gacesa@ansys.com>
2021-12-09 14:24:26 +0100

    [day09] no need for BFS for part 1

--- python/09.py
+++ python/09.py
@@ -15,36 +15,26 @@ DELTAS ={
 def get_neighbors(location, cave_floor):
     return {candidate for candidate in (location + delta for delta in DELTAS) if candidate in cave_floor}
 
-def traverse_floor(cave_floor, cave_heights, start=0+0j, part_2=False):
-    lowpoints = set()
+def find_lowpoints(cave_floor, cave_heights):
+    return {location for location in cave_floor 
+                        if all(cave_heights[neighbor] > cave_heights[location]
+                        for neighbor in get_neighbors(location, cave_floor))}
+
+def traverse_floor(cave_floor, cave_heights, start):
     frontier = deque([start])
     reached = set({start})
-
     while frontier:
         current = frontier.pop()
         current_height = cave_heights[current]
         neighbors = get_neighbors(current, cave_floor)
-        # part 1: find single lowest points
-        if all(cave_heights[neighbor] > current_height for neighbor in neighbors) and not part_2:
-            lowpoints.add(current)
         for neighbor in neighbors:
-            if neighbor not in reached:
-                if not part_2:
-                    frontier.append(neighbor)
-                    reached.add(neighbor)
-                # part 2: find basins
-                elif cave_heights[neighbor] > current_height and cave_heights[neighbor] != 9:
-                        frontier.append(neighbor)
-                        reached.add(neighbor)
-    if part_2:
-        return reached
-    return lowpoints
-
-def part_1(lowpoints):
-    return sum(cave_heights[low_point]+1 for low_point in lowpoints)
+            if neighbor not in reached and cave_heights[neighbor] > current_height and cave_heights[neighbor] != 9:
+                frontier.append(neighbor)
+                reached.add(neighbor)
+    return reached
 
 def part_2(lowpoints, cave_floor, cave_heights):
-    basins = (len(traverse_floor(cave_floor, cave_heights, low_point, True)) for low_point in lowpoints)
+    basins = (len(traverse_floor(cave_floor, cave_heights, low_point)) for low_point in lowpoints)
     return reduce(mul, sorted(basins, reverse=True)[:3])
 
 lines = get_input('inputs/09.txt')
@@ -57,5 +47,9 @@ for row, line in enumerate(lines):
         cave_floor.append(position)
         cave_heights[position] = int(height)
 
-lowpoints = traverse_floor(cave_floor, cave_heights)
-print_solutions(part_1(lowpoints), part_2(lowpoints, cave_floor, cave_heights))
+lowpoints = find_lowpoints(cave_floor, cave_heights)
+
+party_1 = sum(cave_heights[low_point]+1 for low_point in lowpoints)
+party_2 = part_2(lowpoints, cave_floor, cave_heights)
+
+print_solutions(party_1, party_2)
