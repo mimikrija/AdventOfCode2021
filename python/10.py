d16d05809e96b7895a9c6a3b745d29f0d1a43e97
Maja Gacesa <maja.gacesa@ansys.com>
2021-12-10 07:16:48 +0100

    [day10] move input reading into the main part

--- python/10.py
+++ python/10.py
@@ -4,8 +4,6 @@ from santas_little_helpers import *
 from collections import deque
 
 
-lines = get_input('inputs/10.txt')
-
 POINTS = {')': 3, ']': 57, '}': 1197, '>': 25137, None: 0,}
 AUTOCOMPLETE_POINTS = { ')': 1, ']': 2, '}': 3, '>': 4, None: 0,}
 
@@ -36,6 +34,8 @@ def autocomplete(in_line):
     completion = completion.translate(close_it)
     return list(completion)
 
+
+lines = get_input('inputs/10.txt')
 
 analysed_lines = [autocomplete(line) for line in lines]
 
