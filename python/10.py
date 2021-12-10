96391442e313b42add418bf27f5ba6d67a4e440a
Maja Gacesa <maja.gacesa@ansys.com>
2021-12-10 06:13:07 +0100

    [day10] part 1

--- python/10.py
+++ python/10.py
@@ -1,5 +1,39 @@
 from santas_little_helpers import *
+from collections import deque
 
 
 lines = get_input('inputs/10.txt')
-lines = get_input('inputs/10-ex.txt')
+#lines = get_input('inputs/example.txt')
+
+POINTS = {
+    ')': 3,
+    ']': 57,
+    '}': 1197,
+    '>': 25137,
+    None: 0,
+}
+
+MATCHING = {
+    ')': '(',
+    ']': '[',
+    '}': '{',
+    '>': '<',
+}
+
+def find_first_illegal(in_line):
+    chars = deque(in_line)
+    open = deque()
+    while chars:
+        current = chars.popleft()
+        if current in ('([{<'):
+            open.append(current)
+        elif current in (')]}>'):
+            last_opened = open.pop()
+            if MATCHING[current] == last_opened:
+                pass
+            else:
+                return current
+
+
+party_1 = sum(POINTS[find_first_illegal(line)] for line in lines)
+
