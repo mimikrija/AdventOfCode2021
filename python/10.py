d87656123197a052acc6757d29bc50e626d09557
Maja Gacesa <maja.gacesa@ansys.com>
2021-12-10 06:22:52 +0100

    [day10] first go part 2

--- python/10.py
+++ python/10.py
@@ -13,12 +13,29 @@ POINTS = {
     None: 0,
 }
 
+AUTOCOMPLETE_POINTS = {
+    ')': 1,
+    ']': 2,
+    '}': 3,
+    '>': 4,
+    None: 0,
+}
+
+def calculate_score(remaining):
+    score = 0
+    for char in remaining:
+        score *= 5
+        score += AUTOCOMPLETE_POINTS[char]
+    return score
+
 MATCHING = {
     ')': '(',
     ']': '[',
     '}': '{',
     '>': '<',
 }
+
+MATCHING_rev = {op: cl for cl, op in MATCHING.items()}
 
 def find_first_illegal(in_line):
     chars = deque(in_line)
@@ -34,6 +51,26 @@ def find_first_illegal(in_line):
             else:
                 return current
 
+def get_closing(in_line):
+    chars = deque(in_line)
+    open = deque()
+    while chars:
+        current = chars.popleft()
+        if current in ('([{<'):
+            open.append(current)
+        elif current in (')]}>'):
+            last_opened = open.pop()
+            if MATCHING[current] == last_opened:
+                pass
+    return [MATCHING_rev[c] for c in reversed(list(open))]
 
 party_1 = sum(POINTS[find_first_illegal(line)] for line in lines)
 
+incomplete_lines = [line for line in lines if find_first_illegal(line) is None]
+print(len(incomplete_lines))
+
+completion_strings = [get_closing(line) for line in incomplete_lines]
+scores = [calculate_score(remaining) for remaining in completion_strings]
+party_2 = sorted(scores)[len(scores)//2]
+
+print_solutions(party_1, party_2)
