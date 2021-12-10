5ba664651d122664c0dd6d11bfa61b1432d35e18
Maja Gacesa <maja.gacesa@ansys.com>
2021-12-10 06:53:03 +0100

    [day10] cosmetic cleanup

--- python/10.py
+++ python/10.py
@@ -1,55 +1,38 @@
+# Day 10: Syntax Scoring
+
 from santas_little_helpers import *
 from collections import deque
 
 
 lines = get_input('inputs/10.txt')
-#lines = get_input('inputs/example.txt')
 
-POINTS = {
-    ')': 3,
-    ']': 57,
-    '}': 1197,
-    '>': 25137,
-    None: 0,
-}
-
-AUTOCOMPLETE_POINTS = {
-    ')': 1,
-    ']': 2,
-    '}': 3,
-    '>': 4,
-    None: 0,
-}
+POINTS = {')': 3, ']': 57, '}': 1197, '>': 25137, None: 0,}
+AUTOCOMPLETE_POINTS = { ')': 1, ']': 2, '}': 3, '>': 4, None: 0,}
+MATCHING = {')': '(', ']': '[', '}': '{', '>': '<',}
+MATCHING_rev = {op: cl for cl, op in MATCHING.items()}
 
 def calculate_score(remaining):
     score = 0
     for char in remaining:
-        score *= 5
-        score += AUTOCOMPLETE_POINTS[char]
+        score = score*5 + AUTOCOMPLETE_POINTS[char]
     return score
 
-MATCHING = {
-    ')': '(',
-    ']': '[',
-    '}': '{',
-    '>': '<',
-}
-
-MATCHING_rev = {op: cl for cl, op in MATCHING.items()}
-
-def find_first_illegal(in_line):
-    chars = deque(in_line)
-    open = deque()
-    while chars:
-        current = chars.popleft()
+def autocomplete(in_line, is_part_2=False):
+    line = deque(in_line)
+    opens = deque()
+    while line:
+        current = line.popleft()
         if current in ('([{<'):
-            open.append(current)
+            opens.append(current)
         elif current in (')]}>'):
-            last_opened = open.pop()
+            last_opened = opens.pop()
             if MATCHING[current] == last_opened:
                 pass
             else:
+                # incomplete line! return first illegal
                 return current
+    if is_part_2:
+        return [MATCHING_rev[c] for c in reversed(list(opens))]
 
 
 party_1 = sum(POINTS[autocomplete(line)] for line in lines)
