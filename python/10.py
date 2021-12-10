adfcfd1f0cde95caf2b7e78c0c54f9ff9857284c
Maja Gacesa <maja.gacesa@ansys.com>
2021-12-10 07:15:06 +0100

    [day10] use translate instead of dict

--- python/10.py
+++ python/10.py
@@ -8,8 +8,8 @@ lines = get_input('inputs/10.txt')
 
 POINTS = {')': 3, ']': 57, '}': 1197, '>': 25137, None: 0,}
 AUTOCOMPLETE_POINTS = { ')': 1, ']': 2, '}': 3, '>': 4, None: 0,}
-MATCHING = {')': '(', ']': '[', '}': '{', '>': '<',}
-MATCHING_rev = {op: cl for cl, op in MATCHING.items()}
+
+close_it = str.maketrans('([{<>}])', ')]}><{[(')
 
 def calculate_score(remaining):
     score = 0
@@ -17,29 +17,32 @@ def calculate_score(remaining):
         score = score*5 + AUTOCOMPLETE_POINTS[char]
     return score
 
-def autocomplete(in_line, is_part_2=False):
+def autocomplete(in_line):
     line = deque(in_line)
     opens = deque()
     while line:
         current = line.popleft()
-        if current in ('([{<'):
+        if current in '([{<':
             opens.append(current)
-        elif current in (')]}>'):
+        else:
             last_opened = opens.pop()
-            if MATCHING[current] == last_opened:
+            if current.translate(close_it) == last_opened:
                 pass
             else:
                 # incomplete line! return first illegal
                 return current
-    if is_part_2:
-        return [MATCHING_rev[c] for c in reversed(list(opens))]
+    # if line is legal we get here and return list of needed closing chars
+    completion = ''.join(o for o in reversed(opens))
+    completion = completion.translate(close_it)
+    return list(completion)
 
 
-party_1 = sum(POINTS[autocomplete(line)] for line in lines)
+analysed_lines = [autocomplete(line) for line in lines]
 
-incomplete_lines = [line for line in lines if autocomplete(line) is None]
 
-completion_strings = [autocomplete(line, True) for line in incomplete_lines]
+party_1 = sum(POINTS[line] for line in analysed_lines if isinstance(line, str))
+
+completion_strings = (line for line in analysed_lines if isinstance(line, list))
 scores = [calculate_score(remaining) for remaining in completion_strings]
 party_2 = sorted(scores)[len(scores)//2]
 
