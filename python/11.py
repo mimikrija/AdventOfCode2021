84cc486dbc6ffe2befe90583bfecdfc38fc78809
Maja Gacesa <maja.gacesa@ansys.com>
2021-12-11 21:10:51 +0100

    [day11] final cleanup I guess

--- python/11.py
+++ python/11.py
@@ -2,7 +2,6 @@
 
 from santas_little_helpers import *
 
-
 def increase_energy(energy_levels):
     for y, row in enumerate(energy_levels):
         for x, e in enumerate(row):
@@ -11,24 +10,29 @@ def increase_energy(energy_levels):
 def run_step(energy_levels):
     # First, the energy level of each octopus increases by 1.
     increase_energy(energy_levels)
+
     flashed_total = set()
-    # Then, any octopus with an energy level greater than 9 flashes.
     while True:
+    # Then, any octopus with an energy level greater than 9 flashes.
+    # If this causes an octopus to have an energy level greater than 9, it also flashes.
         flashed = {(x, y) for y, row in enumerate(energy_levels) for x, e in enumerate(row)
                             if e > 9 and (x, y) not in flashed_total}
-    # This increases the energy level of all adjacent octopuses by 1 including octopuses that are diagonally adjacent.
+    # This increases the energy level of all adjacent octopuses by 1 
+    # including octopuses that are diagonally adjacent.
         for octopus in flashed:
             for x, y in get_eight_neighbors(*octopus, GRID_SIZE) - flashed:
                     energy_levels[y][x] += 1
-        flashed_total |= flashed
-    # If this causes an octopus to have an energy level greater than 9, it also flashes.
+        flashed_total |= flashed # (An octopus can only flash at most once per step.)
+
     # This process continues as long as new octopuses keep having their energy level increased beyond 9.
-    # (An octopus can only flash at most once per step.)
         if not flashed:
             break
-    # Finally, any octopus that flashed during this step has its energy level set to 0, as it used all of its energy to flash
+
+    # Finally, any octopus that flashed during this step has its energy level set to 0,
+    # as it used all of its energy to flash
     for x, y in flashed_total:
         energy_levels[y][x] = 0
+
     return len(flashed_total)
 
 def solve(energy_levels):
@@ -46,6 +50,4 @@ def solve(energy_levels):
 energy_levels = [[int(num) for num in row] for row in get_input('inputs/example.txt')]
 GRID_SIZE = len(energy_levels)
 
-
 print_solutions(*solve(energy_levels))
-
