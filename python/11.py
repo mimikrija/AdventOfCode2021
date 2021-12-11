4a7d6fdeea59df1dfbd7e1d947e5f163cd7e4640
Maja Gacesa <maja.gacesa@ansys.com>
2021-12-11 18:26:52 +0100

    [day11] part 1 first go

--- python/11.py
+++ python/11.py
@@ -5,20 +5,45 @@ from santas_little_helpers import *
 
 def increase_energy(octopi):
     # First, the energy level of each octopus increases by 1.
-    return {(x, y, E+1) for x, y, E in octopi}
+    for y, row in enumerate(octopi):
+        for x, e in enumerate(row):
+            octopi[y][x] += 1
 
-DELTAS = ()
-
-def get_adjacents(octopi, current_octopus):
+def get_adjacents(current_octopus):
     adjacents = set()
-    for octopus in octopi:
-        if abs(octopus[0] - current_octopus[0]) <= 1 and abs(octopus[1] - current_octopus[1]) <= 1 and octopus != current_octopus:
-            adjacents.add(octopus)
+    x, y = current_octopus
+    for delta_x in (-1, 0, 1):
+        for delta_y in (-1, 0, 1):
+            xn = x + delta_x
+            yn = y + delta_y
+            if 0 <= xn < LIMIT and 0 <= yn < LIMIT and not(xn == x and yn == y):
+                adjacents.add((xn, yn))
     return adjacents
 
-def flash(octopi):
+
+def run_step(energy_levels):
+    flashed_total = set()
     # Then, any octopus with an energy level greater than 9 flashes.
-    flashed = {(x, y, E) for x, y, E in octopi if E > 9}
+    while True:
+        #print(energy_levels)
+        flashed = {(x, y) for y, row in enumerate(energy_levels) for x, E in enumerate(row) if E > 9 and (x, y) not in flashed_total}
+        #print(flashed)
+        for octopus in flashed:
+            for x, y in get_adjacents(octopus):
+                if (x, y) not in flashed:
+                    energy_levels[y][x] += 1
+        #print(energy_levels)
+        flashed_total |= flashed
+        # for x, y in flashed:
+        #     energy_levels[y][x] = 0
+        if not flashed:
+            break
+    for x, y in flashed_total:
+        energy_levels[y][x] = 0
+    return energy_levels, len(flashed_total)
+                
+    print(flashed)
+    quit()
     rest = octopi - flashed
     # This increases the energy level of all adjacent octopuses by 1 including octopuses that are diagonally adjacent.
     adjacents = {adjacent for octopus in flashed for adjacent in get_adjacents(octopi, octopus)}
@@ -28,10 +53,15 @@ def flash(octopi):
     # (An octopus can only flash at most once per step.)
 
 energy_levels = [[int(num) for num in row] for row in get_input('inputs/example.txt')]
-octopi = {(x, y, E) for y, row in enumerate(energy_levels) for x, E in enumerate(row)}
+LIMIT = len(energy_levels)
 
-octopi = increase_energy(octopi)
+party_1 = 0
+for steps in range(100):
+    increase_energy(energy_levels)
+    print('helo')
+    energy_levels, flashes = run_step(energy_levels)
+    print('helo 2')
+    party_1 += flashes
 
-
-flash(octopi)
+print_solutions(party_1)
 
