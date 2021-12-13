48841e46e731474c0ea5727033510d75d2318d94
Maja Gacesa <maja.gacesa@ansys.com>
2021-12-13 08:39:29 +0100

    [day13] generalize folding and cleanup

--- python/13.py
+++ python/13.py
@@ -1,55 +1,41 @@
+# Day 13: Transparent Origami
 
 from santas_little_helpers import *
+
+def fold(coordinates, axis, axis_position):
+    axis_position = int(axis_position)
+    folded = set(coordinates)
+    sym = axis == 'y'
+    same = not sym
+    for coordinate in coordinates:
+        if coordinate[sym] > axis_position:
+            new = 2 * axis_position - coordinate[sym]
+            old = coordinate[same]
+            fold_point = tuple(coord for pos, coord in sorted(zip([sym, same], [new, old])))
+            folded.add(fold_point)
+    return {coordinate for coordinate in folded if coordinate[sym] < axis_position}
+
+def print_from_set(in_set):
+    print()
+    width, heigth = (max(in_set, key=lambda coord:coord[pos])[pos] + 1 for pos in (0, 1))
+    plot_area = [width * [' '] for _ in range(heigth)]
+    for x,y in in_set:
+        plot_area[y][x] = '#'
+    for row in plot_area:
+        print(''.join(c for c in row))
+
+def solve(coordinates, instructions):
+    # follow the folding instructions
+    for step, instruction in enumerate(instructions, start=1):
+        coordinates = fold(coordinates, *instruction)
+        if step == 1:
+            party_1 = len(coordinates)
+    print_solutions(party_1)
+    print_from_set(coordinates)
+
 
 coordinates, instructions = get_input('inputs/13.txt', False, '\n\n')
 coordinates = {tuple(int(line.split(',')[n]) for n in (0, 1)) for line in coordinates.split('\n')}
 instructions = [line.split()[2].split('=') for line in instructions.split('\n')]
 
-def fold(coordinates, axis, number):
-    number = int(number)
-    folded = set(coordinates)
-    if axis == 'y':
-        for x, y in coordinates:
-            if y > number:
-                y = number - (y - number)
-                folded.add((x, y))
-        # remove folding line
-        folded = {coordinate for coordinate in folded if coordinate[1] < number}
-    if axis == 'x':
-        for x, y in coordinates:
-            if x > number:
-                x = number - (x - number)
-                folded.add((x, y))
-        # remove folding line
-        folded = {coordinate for coordinate in folded if coordinate[0] < number}
-    return folded
-
-
-for step, instruction in enumerate(instructions, start=1):
-    coordinates = fold(coordinates, *instruction)
-    if step == 1:
-        party_1 = len(coordinates)
-
-width = heigth = 0
-for x,y in coordinates:
-    if x > width:
-        width = x
-    if y > heigth:
-        heigth = y
-
-width += 1
-heigth +=1
-
-
-def print_from_set(in_set):
-    hull = [width * [' '] for _ in range(heigth)]
-    for x,y in in_set:
-        hull[y][x] = '#'
-    for line in hull:
-        print(''.join(c for c in line))
-
-
-
-print_solutions(party_1)
-
-print_from_set(coordinates)
+solve(coordinates, instructions)
