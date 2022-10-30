2dbb3812bcd5570ae927591e7c4f02259963d5da
Maja Gacesa <maja.gacesa@gmail.com>
2022-10-30 13:09:20 +0100

    instead of stash, this is bad

--- python/22.py
+++ python/22.py
@@ -1,9 +1,8 @@
-from email.policy import default
 from santas_little_helpers.helpers import *
 from re import findall
-from operator import mul, neg
+from operator import mul
 from functools import reduce
-from collections import defaultdict, deque, Counter
+from collections import deque
 
 class Cuboid():
     def __init__(self, borders_x, borders_y, borders_z, on=True):
@@ -46,19 +45,19 @@ class Cuboid():
         return self.volume() <= other.volume()
 
 
-def get_intersection(first, second):
+def get_intersection(first, second, status=False):
     if first == second:
         intersection = first.copy()
-        intersection.switch = False
+        intersection.switch = status
     elif first.all_in(second):
         intersection = first.copy()
-        intersection.switch = False
+        intersection.switch = status
     elif second.all_in(first):
         intersection = second.copy()
-        intersection.switch = False
+        intersection.switch = status
     else:
         # actual intersection
-        intersection = Cuboid((0, 0), (0, 0), (0,0), False)
+        intersection = Cuboid((0, 0), (0, 0), (0,0), status)
         for axis in('x', 'y', 'z'):
             intersection.borders[axis] = (max(first.borders[axis][0], second.borders[axis][0]), min(first.borders[axis][1], second.borders[axis][1]))
 
@@ -78,9 +77,54 @@ def intersect_existing_intersections(negatives, new_intersection):
             new_positive = get_intersection(intersection, new_intersection)
             new_positive.switch = True
             output_pos.append(new_positive)
-    
-    
     return output_pos
+
+def purge_intersections(old_intersections, intersections):
+    # self check current intersections against themselves
+    # mark them as positive
+    if len(intersections) == 1:
+        new_intersections = list(intersections)
+    else:
+        checked = deque([intersections[0]])
+        to_check = deque(intersections[1:])
+        new_intersections = list(intersections)
+        while to_check:
+            attacker = to_check.popleft()
+            for cubo in checked:
+                new_intersections.append(get_intersection(attacker, cubo, True))
+    
+    # purge new intersections from same volumes with different switch
+    # for new1 in new_intersections:
+    #     for new2 in new_intersections[1:]:
+    #         if new1 == new2 and new1.switch != new2.switch:
+    #             new_intersections.remove(new1)
+    #             new_intersections.remove(new2)
+
+    # remove any which "cancel out" (same volume, different switch, no need to keep them in)
+    for old in old_intersections:
+        for new in new_intersections:
+            if old == new and old.switch != new.switch:
+                old_intersections.remove(old)
+                new_intersections.remove(new)
+    
+    # finally, intersect old and new
+    res = []
+    for old in old_intersections:
+        for new in new_intersections:
+            if not old.no_overlap(new):
+                if old.switch and new.switch:
+                    status = False
+                if old.switch != new.switch:
+                    status = False
+                if not old.switch and not new.switch:
+                    status = True
+                res.append(get_intersection(old, new, status))
+
+    return old_intersections + new_intersections + res
+
+    # cancel out positives with the negatives if applicable
+    return purged_intersections
+
 
 
 cuboids = []
@@ -93,7 +137,8 @@ to_check_new = [cuboids[0]]
 remaining = deque(cuboids[1:])
 
 volumes = list(to_check_new)
-
+intersections = []
+existing_intersections = []
 
 while remaining:
     attacker = remaining.popleft()
@@ -101,14 +146,17 @@ while remaining:
     for cuboid in to_check:
         if not cuboid.no_overlap(attacker):
             intersection = get_intersection(cuboid, attacker)
-            volumes.append(intersection)
-            negatives = [volume for volume in volumes if not volume.switch]
+            intersections.append(intersection)
+            #volumes.append(intersection)
+            #negatives = [volume for volume in volumes if not volume.switch]
             
-            volumes += intersect_existing_intersections(negatives, intersection)
+            #volumes += intersect_existing_intersections(negatives, intersection)
+            existing_intersections = purge_intersections(existing_intersections, intersections)
     if attacker.switch:
         volumes.append(attacker)
         to_check_new.append(attacker)
 
+volumes += existing_intersections
 
 
 sign = lambda x: 1 if x else -1
