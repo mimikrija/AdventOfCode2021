07a90e42b58cd6f5d396d71179301a9e96d4ab50
Maja Gacesa <maja.gacesa@gmail.com>
2022-11-01 16:48:03 +0100

    both parts, this needs cleanup
    thanks Reddit. I had figured out the concept of inclusion-exclusion but could not figure out
    how to implement it properly. but it is actually quite simple: if we are intersecting a positive
    cuboid, the intersection should be negative, and vice versa. it does not matter what the attacker is.
    maybe the original messed up implementation would lead to something but playing around with working
    code from someone else I was able to figure out I had an error in `no_overlap` function - I was
    checking for `all` instead of `any` which is why I ended with more intersections than needed.

--- python/22.py
+++ python/22.py
@@ -2,10 +2,10 @@ from santas_little_helpers.helpers import *
 from re import findall
 from operator import mul
 from functools import reduce
-from collections import deque
+from time import time
 
 class Cuboid():
-    def __init__(self, borders_x, borders_y, borders_z, on=True):
+    def __init__(self, borders_x=(0, 0), borders_y=(0, 0), borders_z=(0, 0), on=True):
         self.switch = on
         self.borders = {
             'x': borders_x,
@@ -17,15 +17,13 @@ class Cuboid():
             return True
         return False
 
-
-
     def copy(self):
         return Cuboid(*self.borders.values(), self.switch)
 
     def no_overlap(self, other):
         self_borders = [self.borders[axis] for axis in ('x', 'y', 'z')]
         other_borders = [other.borders[axis] for axis in ('x', 'y', 'z')]
-        return all(self_right < other_left or self_left > other_right for
+        return any(self_right < other_left or self_left > other_right for
             (self_left, self_right), (other_left, other_right) in zip(self_borders, other_borders))
 
     def all_in(self, other):
@@ -36,131 +34,53 @@ class Cuboid():
             (self_left, self_right), (other_left, other_right) in zip(self_borders, other_borders))
 
     def volume(self):
-        return reduce(mul, ((max + 1 - min) for (min, max) in self.borders.values()))
-
-    def __ge__(self, other):
-        return self.volume() >= other.volume()
-
-    def __le__(self, other):
-        return self.volume() <= other.volume()
+        sign = 1 if self.switch else -1
+        return sign*reduce(mul, ((max + 1 - min) for (min, max) in self.borders.values()))
 
 
-def get_intersection(first, second, status=False):
-    if first == second:
+def get_intersection(first, second):
+    
+    if first.no_overlap(second):
+        return None
+    if first == second or first.all_in(second):
         intersection = first.copy()
-        intersection.switch = status
-    elif first.all_in(second):
-        intersection = first.copy()
-        intersection.switch = status
     elif second.all_in(first):
         intersection = second.copy()
-        intersection.switch = status
     else:
         # actual intersection
-        intersection = Cuboid((0, 0), (0, 0), (0,0), status)
+        intersection = Cuboid()
         for axis in('x', 'y', 'z'):
             intersection.borders[axis] = (max(first.borders[axis][0], second.borders[axis][0]), min(first.borders[axis][1], second.borders[axis][1]))
-
+    intersection.switch = not first.switch
     return intersection
 
 
-def intersect_existing_intersections(negatives, new_intersection):
+def switched_on(in_cuboids):
+    intersections = []
+    processed = []
 
-    if not any(intersection.no_overlap(new_intersection) for intersection in negatives):
-        return []
+    for attacker in in_cuboids:
+        intersections = []
 
-    # at least one intersection intersects with the new one, no need to put it in the list, just the intersections
-    # between the existing and the new, and they should be positive (otherwise I'd need to calculate the union)
-    output_pos = []
-    for intersection in negatives:
-        if not intersection.no_overlap(new_intersection):
-            new_positive = get_intersection(intersection, new_intersection)
-            new_positive.switch = True
-            output_pos.append(new_positive)
-    return output_pos
-
-def purge_intersections(old_intersections, intersections):
-    # self check current intersections against themselves
-    # mark them as positive
-    if len(intersections) == 1:
-        new_intersections = list(intersections)
-    else:
-        checked = deque([intersections[0]])
-        to_check = deque(intersections[1:])
-        new_intersections = list(intersections)
-        while to_check:
-            attacker = to_check.popleft()
-            for cubo in checked:
-                new_intersections.append(get_intersection(attacker, cubo, True))
+        for current in processed:
+            intersection = get_intersection(current, attacker)
+            if intersection:
+                intersections.append(intersection)
+        if attacker.switch:
+            processed.append(attacker)
+        processed += intersections
     
-    # purge new intersections from same volumes with different switch
-    # for new1 in new_intersections:
-    #     for new2 in new_intersections[1:]:
-    #         if new1 == new2 and new1.switch != new2.switch:
-    #             new_intersections.remove(new1)
-    #             new_intersections.remove(new2)
+    return sum(cubo.volume() for cubo in processed)
 
-    # remove any which "cancel out" (same volume, different switch, no need to keep them in)
-    for old in old_intersections:
-        for new in new_intersections:
-            if old == new and old.switch != new.switch:
-                old_intersections.remove(old)
-                new_intersections.remove(new)
-    
-    # finally, intersect old and new
-    res = []
-    for old in old_intersections:
-        for new in new_intersections:
-            if not old.no_overlap(new):
-                if old.switch and new.switch:
-                    status = False
-                if old.switch != new.switch:
-                    status = False
-                if not old.switch and not new.switch:
-                    status = True
-                res.append(get_intersection(old, new, status))
-
-    return old_intersections + new_intersections + res
-
-    # cancel out positives with the negatives if applicable
-    return purged_intersections
-
-
-
+# parse intput into list of cuboids
 cuboids = []
-for line in get_input('inputs/22-e.txt'):
+for line in get_input('inputs/22.txt'):
     min_x, max_x, min_y, max_y, min_z, max_z = map(int, findall(r'-?\d+', line))
     is_on = line[:2] == 'on'
     cuboids.append(Cuboid((min_x, max_x), (min_y, max_y), (min_z, max_z), is_on))
 
-to_check_new = [cuboids[0]]
-remaining = deque(cuboids[1:])
 
-volumes = list(to_check_new)
-intersections = []
-existing_intersections = []
+party_1 = switched_on(cuboids[:20])
+party_2 = switched_on(cuboids)
 
-while remaining:
-    attacker = remaining.popleft()
-    to_check = list(to_check_new)
-    for cuboid in to_check:
-        if not cuboid.no_overlap(attacker):
-            intersection = get_intersection(cuboid, attacker)
-            intersections.append(intersection)
-            #volumes.append(intersection)
-            #negatives = [volume for volume in volumes if not volume.switch]
-            
-            #volumes += intersect_existing_intersections(negatives, intersection)
-            existing_intersections = purge_intersections(existing_intersections, intersections)
-    if attacker.switch:
-        volumes.append(attacker)
-        to_check_new.append(attacker)
-
-volumes += existing_intersections
-
-
-sign = lambda x: 1 if x else -1
-total_on = sum(sign(cub.switch)*cub.volume() for cub in volumes)
-
-
-print_solutions(total_on)
+print_solutions(party_1, party_2)
