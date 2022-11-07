c1a3214fc773851a86b281ea2e91f15916fb56d6
Maja Gacesa <maja.gacesa@gmail.com>
2022-11-07 22:43:25 +0100

    [day19] gen. of rotated coordinates, hear me out
    This generator creates some overhead because we yield after each new set
    of coordinates is generated (actually in this moment it is a list of
    coordinates). We keep track of already generated configurations and use
    those to generate further configurations. We first roll, then pitch, and
    finally yaw.
    
    Although if potentially left to yield until last configuration this
    approach generates redundant configurations (worst case scenario we'd
    yield 84 configurations, while only 24 are unique), I think it will
    rarely (if ever) happen simply because we are going to be checking the
    generated configurations for the "12 matching coordinates" rule and at
    that point stop yielding.
    
    My hunch is that optimizing this generator to yield only unique configs
    is not worth it, or that it could even eat up more time.
    
    NEXT STEP: implement the 12 point check between two sets of coordinates:
    - sort both lists
    - generate some sort of map of distances/differences for every coord in
    each list
    - try to find the "pattern" in the other list
    - it pattern matches for 12 or more - return True
    
    I still need to think about this

--- python/19.py
+++ python/19.py
@@ -16,32 +16,40 @@ def overlap(first, second):
 def dotproduct(matrix, vector):
     return(tuple(sum(r*c for r, c, in zip(row, vector)) for row in matrix))
 
+def generate_rotated_coordinates(in_coords):
+    # in the worst case, this yields 4 + 16 + 64 times
+    # which is not ideal, but I am hoping we'll not always need to yield so much
+    # there will be duplicates, of course, but the idea is to just yield until
+    # the first instance od 12 matches is found
 
-
-def rotate_coordinates(in_coord):
-    coord = in_coord
-
-    # keep rolling
+    # first four rolls
+    roll = list(in_coords)
     rolls = []
     for _ in range(4):
-        coord = dotproduct(ROLL, coord)
-        rolls.append(coord)
+        roll = [dotproduct(ROLL, coord) for coord in roll]
+        rolls.append(roll)
+        yield roll
 
-    # keep pitching
-    rollpitchs = []
-    for coord in rolls:
+    # then additional four pitches for each of the rolls
+    pitched_rolls = []
+    for roll in rolls:
+        pitched_roll = roll
         for _ in range(4):
-            coord = dotproduct(PITCH, coord)
-            rollpitchs.append(coord)
+            pitched_roll = [dotproduct(PITCH, coord) for coord in pitched_roll]
+            pitched_rolls.append(pitched_roll)
+            yield pitched_roll
 
-    # keep yawing
-    rollpitchyaws = []
-    for coord in rollpitchs:
+    # finally the four yaws on top of pitched rolls
+    yawed_pitched_rolls = []
+    for pitched_roll in pitched_rolls:
+        yawed_pitched_roll = pitched_roll
         for _ in range(4):
-            coord = dotproduct(YAW, coord)
-            rollpitchyaws.append(coord)
+            yawed_pitched_roll = [dotproduct(YAW, coord) for coord in yawed_pitched_roll]
+            yawed_pitched_rolls.append(yawed_pitched_roll)
+            yield yawed_pitched_roll
 
-    return set(rollpitchyaws)
+
+
 
 
 scanners = dict()
