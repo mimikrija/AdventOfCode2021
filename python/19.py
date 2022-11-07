aa06127d63e823bb11fb22d2080652c1ee85523a
Maja Gacesa <maja.gacesa@gmail.com>
2022-11-07 17:50:12 +0100

    [day19] add code to generate 24 configurations of a coordinate
    this is just for testing the rotation functionality. ideally we would like to generate
    a ful list of coordinates per configuration, and the code I am now committing could serve
    for the opposite - providing a list of configurations per coordinate which is not useful
    for this problem

--- python/19.py
+++ python/19.py
@@ -1,6 +1,10 @@
 from santas_little_helpers.helpers import *
-from itertools import product
+from itertools import product, permutations
 from collections import Counter
+
+ROLL = [(1, 0, 0), (0, 0, -1), (0, 1, 0)]
+PITCH = [(0, 0, 1), (0, 1, 0), (-1, 0, 0)]
+YAW = [(0, -1, 0), (1, 0, 0), (0, 0, 1)]
 
 def diff(first, second):
     return (first[0] - second[0], first[1] - second[1], first[2] - second[2])
@@ -8,6 +12,36 @@ def diff(first, second):
 def overlap(first, second):
     """"Returns most common distance from `second` to `first` coordinates"""
     return Counter((diff(two, one) for one, two in product(first, second))).most_common(1)[0]
+
+def dotproduct(matrix, vector):
+    return(tuple(sum(r*c for r, c, in zip(row, vector)) for row in matrix))
+
+
+
+def rotate_coordinates(in_coord):
+    coord = in_coord
+
+    # keep rolling
+    rolls = []
+    for _ in range(4):
+        coord = dotproduct(ROLL, coord)
+        rolls.append(coord)
+
+    # keep pitching
+    rollpitchs = []
+    for coord in rolls:
+        for _ in range(4):
+            coord = dotproduct(PITCH, coord)
+            rollpitchs.append(coord)
+
+    # keep yawing
+    rollpitchyaws = []
+    for coord in rollpitchs:
+        for _ in range(4):
+            coord = dotproduct(YAW, coord)
+            rollpitchyaws.append(coord)
+
+    return set(rollpitchyaws)
 
 
 scanners = dict()
