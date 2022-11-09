778bb80caea3697b1a9a0bc3477a4e42be20d1e0
Maja Gacesa <maja.gacesa@gmail.com>
2022-11-09 19:52:04 +0100

    [day16] keep track of package versions using a list
    also be very careful. if you call that faunction multiple times, `packet_versions` will
    not be reset to `[]`, but continue from where it left of!!!

--- python/16.py
+++ python/16.py
@@ -4,11 +4,12 @@ from collections import deque
 def to_binary(hex):
     return "".join(f"{byte:08b}" for byte in bytes.fromhex(hex))
 
-def read_packet(packet, total_sum = 0):
+def read_packet(packet, packet_versions=[]):
 
     packet_version = int(''.join(packet.popleft() for _ in range(3)), 2)
     type_id = int(''.join(packet.popleft() for _ in range(3)), 2)
-    total_sum += packet_version
+    packet_versions.append(packet_version)
+
 
     if type_id == 4:
         literal = ''
@@ -16,28 +17,29 @@ def read_packet(packet, total_sum = 0):
             keep_reading = packet.popleft() == '1'
             literal += ''.join(packet.popleft() for _ in range(4))
             if not keep_reading:
-                return int(literal, 2), total_sum
+                break
+                return int(literal, 2), packet_versions
     else:
         length_type_id = packet.popleft()
         if length_type_id == '0':
             total_length = int(''.join(packet.popleft() for _ in range(15)), 2)
             len_diff = len(packet) - total_length
             while True:
-                value, total_number = read_packet(packet)
+                read_packet(packet, packet_versions)
                 if len(packet) <= len_diff:
-                    return value, total_sum + total_number
+                    break
         else:
             number_of_subpackets = int(''.join(packet.popleft() for _ in range(11)), 2)
             for _ in range(number_of_subpackets):
-                value, total_number = read_packet(packet)
-                print('value ', value)
-            return value, total_sum + total_number
+                read_packet(packet, packet_versions)
+
+    return packet_versions
+
+
+data = deque([to_binary(line) for line in get_input('inputs/16.txt')][0])
 
 
 
+party_1 = sum(read_packet(data))
 
-data = [to_binary(line) for line in get_input('inputs/16.txt')]
-
-
-for binary in data:
-    print(read_packet(deque(binary)))
+print_solutions(party_1)
